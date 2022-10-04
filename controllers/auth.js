3a2363724f9ae01dbe41326a1757e642bd1231aa
Julia Tran <julia.kim.tran@hotmail.com>
2022-10-04 19:55:00 -0400

    Hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,19 @@
 
 import User from "../models/User.js"
+import bcrypt from "bcryptjs"
+
 
 export const register = async( req, res, next) => {
  try {
 
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   })
 
   await newUser.save();
