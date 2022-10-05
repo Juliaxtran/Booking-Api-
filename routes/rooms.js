418ea2e9c5b5cd929f7aedc9ec83a89154f2d195
Julia Tran <julia.kim.tran@hotmail.com>
2022-10-05 19:48:56 -0400

    Create room and delete room controllers - verified working

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,10 +1,28 @@
 import express from "express";
 const router = express.Router();
+import { createRoom, deleteRoom, getRoom, updateRoom, getAllRooms} from '../controllers/room.js';
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 
-router.get("/", (req, res) => {
-  res.send("Hello this is rooms endpoint");
-});
 
+
+
+// Create
+router.post("/:hotelId", verifyAdmin, createRoom);
+
+// Update
+router.put("/:id", verifyAdmin,  updateRoom);
+
+// Delete
+
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+
+// Get
+
+router.get("/:id", getRoom);
+
+// Get All
+
+router.get("/", getAllRooms);
 
 export default router
